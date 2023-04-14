8dec36d58a105e8176032fcabbc4c4423f5dcc5f
anabelard <anabela.rosanti@ti.ukdw.ac.id>
2023-04-14 21:28:44 +0700

    Update english.py

--- english.py
+++ english.py
@@ -1,10 +1,36 @@
 import os
-eng = os.chdir('english')
-engFiles = os.listdir(eng)
+import nltk
 
-for j in engFiles:
-    if j.endswith('.txt'):
-        files = open(j,'r')
-       
-        print(files.read())
+from nltk import *
+from nltk.corpus import stopwords
 
+english = os.chdir('english')
+englishFiles = os.listdir(english)
+
+for i in englishFiles:
+    if i.endswith('.txt'):
+        files = open(i,'r').read()
+
+        lower = files.lower()
+
+        stopword = set(stopwords.words("english"))
+
+        regex =  RegexpTokenizer('\W+', gaps = True)
+        tokenizedWords = regex.tokenize(lower)
+
+        englishToken = []
+        for words in tokenizedWords:
+            if words not in stopword:
+                englishToken.append(words)
+        
+        uni_b = {}
+
+        for x in englishToken:
+            if x not in uni_b:
+                uni_b[x] = 1
+            else:
+                uni_b[x] += 1
+
+
+        for words,freq in uni_b.items():
+            print("{} : {}".format(words,freq))
